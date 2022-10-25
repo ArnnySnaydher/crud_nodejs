eb0ccfc5cf46ca0b0a409bce19b83024ceace54d
Arnny Snaydher Quispe Loayza <quispeloayza18@hotmail.com>
2022-10-24 21:56:56 -0500

    second commit

--- router.js
+++ router.js
@@ -1,12 +1,15 @@
 const {Router} = require('express');
 const router = Router();
+const conexion = require('./database/db')
 
-router.get('/contacto', (req,res) => {
-    res.send("persona");
-})
+// router.get('/contacto', (req,res) => {
+//     res.send("persona");
+// })
 
-router.get('/pais', (req,res) => {
-    res.send("peru");
+router.get('/', (req,res) => {
+    conexion.query('SELECT * FROM users',(Error,reults)=>{
+        
+    })
 })
 
 module.exports = router;
